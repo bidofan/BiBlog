f1a2a8371ddcfce9c76b54fbc586acd6d52bcef1
vasuok <sbidochka@mail.ru>
2018-01-12 13:48:38 +0200

    pushed project

---
+++ AdaptiveSite/js/index.js
@@ -0,0 +1,8 @@
+menu.onclick = function  myFunc() {
+  var x = document.getElementById("Topnav");
+  if(x.className === "topnav"){
+    x.className += " responsive";
+  }else{
+    x.className = "topnav";
+  }
+}
